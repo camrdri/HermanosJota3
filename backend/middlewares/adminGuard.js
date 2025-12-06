fa5fce26043b13e31872d4e02f030a22e955f062
gabi2704 <gabrielatapia2704@gmail.com>
2025-12-06 11:37:53 -0300

    elimino adminGuard

--- backend/middlewares/adminGuard.js
+++
@@ -1,6 +0,0 @@
-module.exports = function adminGuard(req, res, next) {
-  if (!req.user?.rol?.includes("admin")) {
-    return res.status(403).json({ message: "Acceso denegado" });
-  }
-  next();
-};
