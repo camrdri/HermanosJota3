fd95a44ecb4748ab4b7de5a3fe4738bac446ac8f
gabi2704 <gabrielatapia2704@gmail.com>
2025-12-06 11:42:12 -0300

    index

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,18 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { CartProvider } from './context/CartContext';
-import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </AuthProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
