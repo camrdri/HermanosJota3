e897131e0eab6ca187907f7d4e7526aa510a9946
camrdri <camlurodri@gmail.com>
2025-12-07 19:51:23 -0300

    Username

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
+  username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }, // Se guardará encriptada
+  rol: { type: [String], default: ["cliente"] },
   fechaRegistro: { type: Date, default: Date.now }
 });
 
